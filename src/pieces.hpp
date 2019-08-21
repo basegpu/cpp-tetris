982631a466f6c70efd207b9d20dfee686cd8a6dd
Samuel Peter <sapeter@ethz.ch>
2019-08-21 02:16:51 +0200

    optimize pieces

--- src/pieces.hpp
+++ src/pieces.hpp
@@ -5,19 +5,13 @@
 #define WIDTH 5
 #define NROTATIONS 4
 
-typedef struct
-{
-	int x;
-	int y;
-} Index;
-
 
 class Piece
 {
 public:
 	Piece(const char (&asset)[WIDTH][WIDTH])
 	{
-		this->rotate<NROTATIONS-1>(asset);
+		this->rotateRecursion<NROTATIONS-1>(asset);
 	}
 
 	char getShape(const int& rotation, const int& x, const int& y) const
@@ -37,50 +31,58 @@ private:
 	char assets[NROTATIONS][WIDTH][WIDTH];
 
 	template<int ROTATION>
-	void rotate(const char (&asset)[WIDTH][WIDTH])
+	void rotateRecursion(const char (&asset)[WIDTH][WIDTH])
 	{
 		this->doRotation<ROTATION>(asset);
-		this->rotate<ROTATION-1>(asset);
+		this->rotateRecursion<ROTATION-1>(asset);
 	}
 
-	template<int ROTATE>
+	template<int ROTATION>
 	void doRotation(const char (&asset)[WIDTH][WIDTH])
 	{
-		this->initAsset<WIDTH*WIDTH-1>(asset, ROTATE);
+		this->assetInitializationRecursion<WIDTH*WIDTH-1>(asset, ROTATION);
 	}
 
-	template<int ISHAPE>
-	void initAsset(const char (&asset)[WIDTH][WIDTH], const int& rotation)
+	template<int INDEX>
+	void assetInitializationRecursion(const char (&asset)[WIDTH][WIDTH], const int& rotation)
 	{
-		int ii = ISHAPE / WIDTH;
-		int jj = ISHAPE % WIDTH;
-		Index index = this->getRotatedIndices(ii, jj, rotation);
-		this->assets[rotation][index.x][index.y] = asset[ii][jj];
-		this->initAsset<ISHAPE-1>(asset, rotation);
+		this->doAssetInitialization<INDEX>(asset, rotation);
+		this->assetInitializationRecursion<INDEX-1>(asset, rotation);
 	}
 
-	Index getRotatedIndices(const int& x, const int& y, const int& rotation)
+	template<int INDEX>
+	void doAssetInitialization(const char (&asset)[WIDTH][WIDTH], const int& rotation)
 	{
+		int ii = INDEX / WIDTH;
+		int jj = INDEX % WIDTH;
+		char v = asset[ii][jj];
+		this->rotateIndices(ii, jj, rotation);
+		this->assets[rotation][ii][jj] = v;
+	}
+
+	void rotateIndices(int& x, int& y, const int& rotation)
+	{
+		int ii = x;
+		int jj = y;
 		switch (rotation)
 		{
-			case 0: return {x, y};
-			case 1: return {y, WIDTH-1-x};
-			case 2: return {WIDTH-1-x, WIDTH-1-y};
-			case 3: return {WIDTH-1-y, x};
+			case 0: break;
+			case 1: x = jj; y = WIDTH-1-ii; break;
+			case 2: x = WIDTH-1-ii; y = WIDTH-1-jj; break;
+			case 3: x = WIDTH-1-jj, y = ii; break;
 			default: throw std::runtime_error("bad rotation");
 		}
 	}	
 };
 
 template<>
-void Piece::rotate<0>(const char (&asset)[WIDTH][WIDTH])
+void Piece::rotateRecursion<0>(const char (&asset)[WIDTH][WIDTH])
 {
 	this->doRotation<0>(asset);
 }
 
 template<>
-void Piece::initAsset<0>(const char (&asset)[WIDTH][WIDTH], const int& rotation)
+void Piece::assetInitializationRecursion<0>(const char (&asset)[WIDTH][WIDTH], const int& rotation)
 {
-	Index index = this->getRotatedIndices(0, 0, rotation);
-	this->assets[rotation][index.x][index.y] = asset[0][0];
+	this->doAssetInitialization<0>(asset, rotation);
 }
