564de15f182c07d80be6e0cea7b368b02e9ce324
Samuel Peter <sapeter@ethz.ch>
2019-08-27 20:37:24 +0200

    add globals definition

---
+++ src/include/globals.hpp
@@ -0,0 +1,18 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+#define TETRIMINO_WIDTH 4
+#define TETRIMINO_NROTATIONS 4
+
+#define BOARD_WIDTH 10              // Board width in blocks
+#define BOARD_HEIGHT 20             // Board height in blocks
+
+// typedefs
+
+enum class Position : unsigned char
+{
+    Free,   // free position of the board
+    Filled  // filled position of the board
+};
+
+#endif
